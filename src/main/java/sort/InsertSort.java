6802f6e591fa3cf81633fd35233e6c8703a5a10d
shuaijun.yang <shuaijun.yang@ucarinc.com>
2020-08-03 16:32:12 +0800

    排序算法

--- src/main/java/sort/InsertSort.java
+++ src/main/java/sort/InsertSort.java
@@ -4,7 +4,8 @@ import com.alibaba.fastjson.JSON;
 
 /**
  * Description: 插入排序
- *
+ * 将排序数据分为已排序区间和未排序区间，依次遍历未排序区间，和已排序区间的值进行比较，
+ * 将其插入到合适的位置上，直至将未排序的区间数据遍历完
  * @author : 杨帅军 (shuaijun.yang@ucarinc.com)
  * @since : 2020/8/3 14:01:22
  **/
@@ -23,6 +24,7 @@ public class InsertSort {
             }
         }
     }
+
 
     public static void main(String[] args) {
         int[] temp  = {1,5,3,4,7,6,9,2};
--- src/main/java/sort/SelectSort.java
+++ src/main/java/sort/SelectSort.java
@@ -4,7 +4,8 @@ import com.alibaba.fastjson.JSON;
 
 /**
  * Description: 选择排序
- *
+ * 将排序的数据分为已排序区间和未排序区间，一般是以第一个元素为已排序区间，然后依次遍历未排序区间，找到其最小值，
+ * 和未排序区间的最后一个值进行比较，交换位置。未排序区间遍历完毕，则排序结束
  * @author : 杨帅军 (shuaijun.yang@ucarinc.com)
  * @since : 2020/8/3 13:58:39
  **/
@@ -25,6 +26,7 @@ public class SelectSort {
             }
         }
     }
+
 
     public static void main(String[] args) {
         int[] temp  = {1,5,3,4,7,6,9,2};
